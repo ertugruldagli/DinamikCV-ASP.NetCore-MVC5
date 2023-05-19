b234a5363fafed921dd9d189966b5c1a88a7a6da
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-19 13:22:35 +0300

    some changes

--- DinamikCV/DinamikCV/Controllers/DefaultController.cs
+++ DinamikCV/DinamikCV/Controllers/DefaultController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DinamikCV.Models.Entity;
 
 namespace DinamikCV.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        DBCVEntities db = new DBCVEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.tblHakkimda.ToList();
+
+            return View(degerler);
         }
     }
 }
