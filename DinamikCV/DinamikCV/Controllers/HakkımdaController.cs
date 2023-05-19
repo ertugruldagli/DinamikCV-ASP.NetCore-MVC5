189d24aac3f8210e6eb162880ded86600316a82c
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-19 18:51:40 +0300

    some changes

---
+++ DinamikCV/DinamikCV/Controllers/HakkımdaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DinamikCV.Controllers
+{
+    public class HakkımdaController : Controller
+    {
+        // GET: Hakkımda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
