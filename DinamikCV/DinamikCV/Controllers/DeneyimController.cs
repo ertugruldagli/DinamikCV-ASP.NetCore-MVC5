ecf64c8d0ce8ce99bf8386b324b80821d6062167
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-22 11:35:11 +0300

    some changes

---
+++ DinamikCV/DinamikCV/Controllers/DeneyimController.cs
@@ -0,0 +1,33 @@
+﻿using DinamikCV.Models.Entity;
+using DinamikCV.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DinamikCV.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+        DeneyimRepository dRepo =new DeneyimRepository();
+        public ActionResult Index()
+        {
+            var degerler = dRepo.TList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult DeneyimEkle() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DeneyimEkele(tblDeneyimlerim p)
+        {
+           dRepo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
