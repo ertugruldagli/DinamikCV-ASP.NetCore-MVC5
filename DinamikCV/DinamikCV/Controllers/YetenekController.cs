82a51fc840036296a3800e88efb1f2af990d4bbb
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-23 10:54:44 +0300

    some changes

--- DinamikCV/DinamikCV/Controllers/YetenekController.cs
+++ DinamikCV/DinamikCV/Controllers/YetenekController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DinamikCV.Models.Entity;
+using DinamikCV.Repositories;
 
 namespace DinamikCV.Controllers
 {
     public class YetenekController : Controller
     {
         // GET: Yetenek
+
+        GenericRepository<tblYeteneklerim> yRepo = new GenericRepository<tblYeteneklerim>();
+
+
         public ActionResult Index()
         {
-            return View();
+            var degerler = yRepo.TList();
+            return View(degerler);
         }
     }
 }
