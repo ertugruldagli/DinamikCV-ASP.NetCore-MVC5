25af6a6efb3f9a592f3c75baeb23c37de1903477
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-23 20:43:53 +0300

    some changes

---
+++ DinamikCV/DinamikCV/Controllers/EgitimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DinamikCV.Models.Entity;
+using DinamikCV.Repositories;
+
+namespace DinamikCV.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<tblEgitimlerim> eRepo = new GenericRepository<tblEgitimlerim>();
+        public ActionResult Index()
+        {
+            var egitimlerim = eRepo.TList();
+            return View(egitimlerim);
+        }
+    }
+}
