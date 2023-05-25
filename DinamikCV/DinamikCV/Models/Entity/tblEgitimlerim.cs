35f77d75a070312b8eb285cdb9b71ef3a88f5818
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-25 09:38:37 +0300

    some changes

--- DinamikCV/DinamikCV/Models/Entity/tblEgitimlerim.cs
+++ DinamikCV/DinamikCV/Models/Entity/tblEgitimlerim.cs
@@ -11,11 +11,13 @@ namespace DinamikCV.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class tblEgitimlerim
     {
         public int ID { get; set; }
         public string Baslik { get; set; }
+        [Required (ErrorMessage ="Boþ Geçemezsiniz!")]
         public string AltBaslik1 { get; set; }
         public string AltBaslik2 { get; set; }
         public string GNO { get; set; }
