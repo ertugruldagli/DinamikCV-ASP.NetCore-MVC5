ab049f2caab1ccec9c7c74700caa5f33e6d10763
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-26 10:04:54 +0300

    part 7 the son

--- DinamikCV/DinamikCV/Controllers/EgitimController.cs
+++ DinamikCV/DinamikCV/Controllers/EgitimController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using DinamikCV.Models.Entity;
@@ -31,6 +32,33 @@ namespace DinamikCV.Controllers
             }
             eRepo.TAdd(p);
             return RedirectToAction ("Index");  
+        }
+        [HttpGet]
+        public ActionResult EgitimGuncelle(int id)
+        {
+            tblEgitimlerim t = eRepo.Find(x => x.ID == id);
+            return View(t);
+
+        }
+        [HttpPost]
+        public ActionResult EgitimGuncelle(tblEgitimlerim p)
+        {
+            tblEgitimlerim t = eRepo.Find(x => x.ID == p.ID);
+            t.ID = p.ID;
+            t.Baslik=p.Baslik;
+            t.AltBaslik1=p.AltBaslik1;
+            t.AltBaslik2=p.AltBaslik2;
+            t.Tarih=t.Tarih;
+            eRepo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult EgitimSil(int id)
+        {
+            var delete = eRepo.Find(x => x.ID == id);
+            eRepo.TRemove(delete);
+
+            return RedirectToAction("Index");
         }
     }
 }
