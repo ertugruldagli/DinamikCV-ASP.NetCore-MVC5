2e7b6f5fbb3413f83c4960f9604f096edb66304e
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-28 10:45:00 +0300

    create to sertifikaFolder

---
+++ DinamikCV/DinamikCV/Controllers/SertifikaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DinamikCV.Controllers
+{
+    public class SertifikaController : Controller
+    {
+        // GET: Sertifika
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
