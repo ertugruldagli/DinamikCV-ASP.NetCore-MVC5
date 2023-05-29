6b0721d0d4e5ad1697d00c795a8905c7aa8ca0ee
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-29 14:49:29 +0300

    some changes

---
+++ DinamikCV/DinamikCV/Controllers/HakkimdaController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DinamikCV.Models.Entity;
+using DinamikCV.Repositories;
+
+
+namespace DinamikCV.Controllers
+{
+    public class HakkimdaController : Controller
+    {
+        // GET: Hakkımda
+        GenericRepository<tblHakkimda> hRepo=new GenericRepository<tblHakkimda>();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hakkimda = hRepo.TList();
+            return View(hakkimda);
+        }
+        [HttpPost]
+        public ActionResult Index(tblHakkimda p)
+        {
+            var hakkimda = hRepo.Find(x=>x.ID==1);
+            hakkimda.Ad=p.Ad;
+            hakkimda.Soyad=p.Soyad;
+            hakkimda.Adres=p.Adres;
+            hakkimda.Telefon=p.Telefon;
+            hakkimda.Mail=p.Mail;   
+            hakkimda.Resim=p.Resim;
+            hRepo.TUpdate(hakkimda);
+            return RedirectToAction("Index");
+        }
+    }
+}
