da14424c5894f11699272ab4113bf9b68f783b8e
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-29 13:42:50 +0300

    some changces

---
+++ DinamikCV/DinamikCV/Controllers/iletisimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DinamikCV.Controllers
+{
+    public class iletisimController : Controller
+    {
+        // GET: Ilet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
