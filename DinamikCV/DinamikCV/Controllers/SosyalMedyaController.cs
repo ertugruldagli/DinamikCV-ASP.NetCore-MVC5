1454fad523c6d01e65fe6b29156055bf5e1e4955
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-30 11:06:10 +0300

    some changes

---
+++ DinamikCV/DinamikCV/Controllers/SosyalMedyaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DinamikCV.Models.Entity;
+using DinamikCV.Repositories;
+
+namespace DinamikCV.Controllers
+{
+    public class SosyalMedyaController : Controller
+    {
+        // GET: SosyalMedya
+        GenericRepository<tblSosyalMedya> sRepo=new GenericRepository<tblSosyalMedya>();
+        public ActionResult Index()
+        {
+            var sosyalmedya = sRepo.TList();
+            return View (sosyalmedya);
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(tblSosyalMedya p)
+        {
+            sRepo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
