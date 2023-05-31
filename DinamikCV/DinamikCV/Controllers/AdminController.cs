5b0799b9b963096b9b0bea6e593542a2f3901456
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-05-31 13:44:13 +0300

    some changces

---
+++ DinamikCV/DinamikCV/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DinamikCV.Models.Entity;
+using DinamikCV.Repositories;
+
+namespace DinamikCV.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller
+    {
+
+        GenericRepository<tblAdmin> aRepo=new GenericRepository<tblAdmin>();
+        // GET: Admin
+       
+        public ActionResult Index()
+        {
+            var veriler = aRepo.TList();
+            return View(veriler);
+        }
+        [HttpGet]
+        public ActionResult AdminEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminEkle(tblAdmin p)
+        {
+            aRepo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AdminSil(int id)
+        {
+            tblAdmin t = aRepo.Find(x => x.ID == id);
+            aRepo.TRemove(t);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AdminDuzenle(int id)
+        {
+            tblAdmin t = aRepo.Find(x => x.ID == id);
+            return View(t);
+
+        }
+        [HttpPost]
+        public ActionResult AdminDuzenle(tblAdmin p)
+        {
+            tblAdmin t = aRepo.Find(x => x.ID == p.ID);
+            t.ID = p.ID;
+            t.KullaniciAdi = p.KullaniciAdi;
+            t.Sifre = p.Sifre;
+           
+            aRepo.TUpdate(t);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
